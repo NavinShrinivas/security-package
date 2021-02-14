c04535853bc3c0952284df6cd0605033beb7c165
Navin Shrinivas <karupal2002@gmail.com>
2021-02-14 07:35:44 +0530

    fixed bugs from previous commit

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 from os import system, name
 from passripper import generate
 
-#=============================decorators==================
-def clearer(function):
+def clearer():
+    if(name=="nt"): #checks for windows device
+        _ = system('cls')
+    else: #linux and mac
+        _ =system("clear")
 
-    #------------------------------------------
-        if(name=="nt"): #checks for windows device
-            _ = system('cls')
-        else: #linux and mac
-            _ =system("clear")
-    #------------------------------------------
-        function()
-    #------------------------------------------
-        if(name=="nt"): #checks for windows device
-            _ = system('cls')
-        else: #linux and mac
-            _ =system("clear")
-    #------------------------------------------
-
-
-#=============================functions=========================
-@clearer
 def dbcreate():
     dbname=input("Enter name of DB: ")
     f=open(dbname+".txt","w+")
@@ -29,24 +15,18 @@ def dbcreate():
     print("PASSWORD FOR CREATED DB [PLEASE REMEMBER CAREFULLY][CAN NOT BE RESET]:")
     print("\t \t",password)
     waste=input("Press ENTER to go back to menu")
-#=====================================================================
+    
 
 
-#------------------------------------------
-if(name=="nt"): #checks for windows device
-    _ = system('cls')
-else: #linux and mac
-    _ =system("clear")
-#------------------------------------------
-
+clearer()
 flag=True
-
 while flag:
     print("==========PASSWORD MANAGER==========")
     print("1.new DB")
     print("2.existing DB")
     print("3.Exit")
     choice=int(input("Enter your choice: "))
-
-    if(choice==1):
+    if choice==1:
+        clearer()
         dbcreate()
+        clearer()
